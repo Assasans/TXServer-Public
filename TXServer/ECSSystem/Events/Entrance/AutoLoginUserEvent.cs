5d7ead4c8a424c6a9e52a4d301f9afac98e3444a
s809 <NoNick@dev.null>
2020-07-12 20:00:03 +0500

    Fixed config location Added auto-login

---
+++ TXServer/ECSSystem/Events/Entrance/AutoLoginUserEvent.cs
@@ -0,0 +1,23 @@
+﻿using TXServer.Core;
+using TXServer.Core.Protocol;
+using TXServer.ECSSystem.Base;
+
+namespace TXServer.ECSSystem.Events
+{
+    [SerialVersionUID(1438075609642)]
+    public class AutoLoginUserEvent : ECSEvent
+    {
+        public void Execute(Entity clientSession)
+        {
+            Player.Instance.Uid = Uid;
+
+            new LoginByPasswordEvent().Execute(clientSession);
+        }
+
+        public string Uid { get; set; }
+
+        public byte[] EncryptedToken { get; set; }
+
+        public string HardwareFingerprint { get; set; }
+    }
+}
