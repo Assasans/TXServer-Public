1429437f9ef1d3fc6cbe84b822410db8fb956e49
M8te <m8@dev.null>
2021-03-07 23:20:47 +0100

    Added RU translation to BanUserEvent
    Syntax: use "help" command in admin console

--- TXServer/ECSSystem/Events/Battle/ElevatedAccess/ElevatedAccessUserBanUserEvent.cs
+++ TXServer/ECSSystem/Events/Battle/ElevatedAccess/ElevatedAccessUserBanUserEvent.cs
@@ -18,6 +18,14 @@ namespace TXServer.ECSSystem.Events.Battle
 			Core.Battles.Battle battle = ServerConnection.BattlePool.Single(b => b.MatchPlayers.Contains(player.BattleLobbyPlayer));
 			Player punishedPlayer = null;
 
+			List<string> availableUnits = new() { "MINUTE", "HOUR", "DAY", "WARN" };
+			if (!availableUnits.Contains(Regex.Replace(Type, "[0-9]", "").Replace("S", "")) && !String.IsNullOrEmpty(Type))
+			{
+				string errorMessage = $"Private message: Invalid/No unit found in paramter '{Type}'";
+				sendErrorMessage(player, battle, errorMessage);
+				return;
+			}
+
 			string uid = "";
 			foreach (BattleLobbyPlayer battleLobbyPlayer in battle.MatchPlayers)
             {
@@ -32,21 +40,10 @@ namespace TXServer.ECSSystem.Events.Battle
 			string message = string.Empty;
 			if (punishedPlayer != null) {
 				string duration = string.Empty;
+				string durationIfNeeded = "";
 				string unit = string.Empty;
 				string punishment = string.Empty;
-				Dictionary<string, string> punishmentsEN = new Dictionary<string, string>
-				{
-					{ "WARN", "warned" },
-					{ "MINUTE", "chat banned" },
-					{ "HOUR", "chat banned" },
-					{ "DAY", "chat banned" }
-				};
-				Dictionary<string, string> unitsEN = new Dictionary<string, string>
-				{
-					{ "MINUTE", "minute" },
-					{ "HOUR", "hour" },
-					{ "DAY", "day" }
-				};
+				List<string> punishments = new();
 
 				for (int i=0; i< Type.Length; i++)
                 {
@@ -56,20 +53,82 @@ namespace TXServer.ECSSystem.Events.Battle
 				if (string.IsNullOrEmpty(duration))
 					duration = "0";
 
-				string chatLanguage = "en";
+				string chatLanguage = "ru";
 				if (chatLanguage == "en")
                 {
-					punishment = punishmentsEN[Regex.Replace(Type, "[0-9]", "").Replace("S", "")];
+					punishments.AddRange(new List<string>() { "warned", "chat banned" });
 
-					unit = unitsEN[Regex.Replace(Type, "[0-9]", "").Replace("S", "")];
+					unit = Regex.Replace(Type, "[0-9]", "").Replace("S", "").ToLower();
 					if (Convert.ToInt32(duration) > 1)
 						unit += "s";
-
-					message = $"{Uid} was {punishment} for {duration} {unit}. Reason: {Reason}";
 				}
+				else
+                {
+					punishments.AddRange(new List<string>() { "предупрежден", "отключен от чата" });
 
-				if (string.IsNullOrEmpty(message))
-					return;
+					int intDuration = Convert.ToInt32(duration);
+					List<string> dictUnit = new();
+					if (Type.Replace("S", "") == "MINUTE")
+                    {
+						dictUnit.Add("минуту");
+						dictUnit.Add("минуты");
+						dictUnit.Add("минут");
+                    }
+					else if (Type.Replace("S", "") == "HOUR")
+					{
+						dictUnit.Add("час");
+						dictUnit.Add("часа");
+						dictUnit.Add("часов");
+					}
+
+					if (dictUnit.Any())
+                    {
+						unit = intDuration switch
+						{
+							1 => dictUnit[0],
+							int n when n >= 2 && n <= 4 => dictUnit[1],
+							_ => dictUnit[2],
+						};
+					}
+					else
+                    {
+                        unit = intDuration switch
+                        {
+                            1 => "сутки",
+                            _ => "суток",
+                        };
+                    }
+				}
+				punishment = Regex.Replace(Type, "[0-9]", "").Replace("S", "") switch
+				{
+					"WARN" => punishments[0],
+					_ => punishments[1],
+				};
+
+				if (chatLanguage == "en")
+				{
+					if (Type != "WARN")
+					{
+                        durationIfNeeded = Type switch
+                        {
+                            "FOREVER" or "" => " forever",
+                            _ => $" for {duration} {unit}",
+                        };
+                    }
+					message = $"{Uid} was {punishment}{durationIfNeeded}. Reason: {Reason}";
+				}
+				else
+                {
+					if (Type != "WARN")
+					{
+                        durationIfNeeded = Type switch
+                        {
+                            "FOREVER" or "" => " навсегда",
+                            _ => $" на {duration} {unit}",
+                        };
+                    }
+					message = $"{Uid} был {punishment}{durationIfNeeded}. Причина: {Reason}";
+				}
 
 				battle.MatchPlayers.Select(x => x.Player).SendEvent(new ChatMessageReceivedEvent
 				{
@@ -83,16 +142,21 @@ namespace TXServer.ECSSystem.Events.Battle
 			else
             {
 				message = $"Private message: {Uid} wasn't found in this battle";
-				player.SendEvent(new ChatMessageReceivedEvent
-				{
-					Message = message,
-					SystemMessage = true,
-					UserId = player.User.EntityId,
-					UserUid = player.User.GetComponent<UserUidComponent>().Uid,
-					UserAvatarId = player.User.GetComponent<UserAvatarComponent>().Id
-				}, battle.GeneralBattleChatEntity);
+				sendErrorMessage(player, battle, message);
             }
         }
 		public string Type { get; set; }
+
+		static void sendErrorMessage(Player player, Core.Battles.Battle battle, string message)
+        {
+			player.SendEvent(new ChatMessageReceivedEvent
+			{
+				Message = message,
+				SystemMessage = true,
+				UserId = player.User.EntityId,
+				UserUid = player.User.GetComponent<UserUidComponent>().Uid,
+				UserAvatarId = player.User.GetComponent<UserAvatarComponent>().Id
+			}, battle.GeneralBattleChatEntity);
+		}
 	}
 }
