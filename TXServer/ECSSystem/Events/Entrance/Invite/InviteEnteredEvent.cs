4590173aa01e63b09dc28cf7d453e824b81eb988
s809 <NoNick@dev.null>
2021-03-30 00:24:50 +0500

    Add invited nicknames part 3

--- TXServer/ECSSystem/Events/Entrance/Invite/InviteEnteredEvent.cs
+++ TXServer/ECSSystem/Events/Entrance/Invite/InviteEnteredEvent.cs
@@ -13,10 +13,13 @@ namespace TXServer.ECSSystem.Events.Entrance.Invite
 		public void Execute(Player player, Entity entity)
 		{
 			// Todo: database immigration
-			List<string> AllowedUids = new() { "NoNick","Tim203", "M8", "Kaveman", "Concodroid", "Corpserdefg",
+			List<string> AllowedUids = new()
+			{
+				"NoNick", "Tim203", "M8", "Kaveman",
+				"Concodroid", "Corpserdefg",
 				"SH42913",
 				"Bodr", "C6OI", "Legendar-X", "Pchelik", "networkspecter", "DageLV",
-				"Black_Wolf", "NN77296", "MEWPASCO_l_WolfCat"
+				"Black_Wolf", "NN77296", "MEWPASCO_l_WolfCat", "Doctor"
 			};
 			string inviteCode = entity.GetComponent<InviteComponent>().InviteCode;
 			if (AllowedUids.Contains(inviteCode) || AllowedUids.Contains(inviteCode.ToLower()))
