a2851b1388f69405a4d51f005067d49b915c3e77
Daniil Pryima <swimmin2@gmail.com>
2021-04-10 18:21:51 +0300

    User context menu
    Temporary solution to fix crashes on `UserInteractionDataRequestEvent`

---
+++ TXServer/ECSSystem/Events/User/UserInteractionDataRequestEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using TXServer.Core;
+using TXServer.Core.Protocol;
+using TXServer.ECSSystem.Base;
+
+namespace TXServer.ECSSystem.Events
+{
+	[SerialVersionUID(1454623211245)]
+	public class UserInteractionDataRequestEvent : ECSEvent
+	{
+		public long UserId { get; set; }
+
+		public void Execute(Player player, Entity entity)
+		{
+			Player targetPlayer = player.Server.FindPlayerById(UserId);
+			player.SendEvent(new UserInteractionDataResponseEvent(
+				targetPlayer.User.EntityId,
+				targetPlayer.Data.UniqueId,
+				true,
+				false,
+				false,
+				false
+			), entity);
+		}
+	}
+}
---
+++ TXServer/ECSSystem/Events/User/UserInteractionDataResponseEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+using TXServer.Core.Protocol;
+using TXServer.ECSSystem.Base;
+
+namespace TXServer.ECSSystem.Events
+{
+	[SerialVersionUID(1412360987645)]
+	public class UserInteractionDataResponseEvent : ECSEvent
+	{
+		public long UserId { get; set; }
+
+		public string UserUid { get; set; }
+
+		public bool CanRequestFrendship { get; set; }
+
+		public bool FriendshipRequestWasSend { get; set; }
+
+		public bool Muted { get; set; }
+
+		public bool Reported { get; set; }
+
+		public UserInteractionDataResponseEvent(long userId, string userUid, bool canRequestFrendship, bool friendshipRequestWasSend, bool muted, bool reported)
+		{
+			UserId = userId;
+			UserUid = userUid;
+
+			CanRequestFrendship = canRequestFrendship;
+			FriendshipRequestWasSend = friendshipRequestWasSend;
+
+			Muted = muted;
+
+			Reported = reported;
+		}
+	}
+}
