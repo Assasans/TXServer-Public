9db86036699bdee34be2fe03df15b759a0d7d8a5
Daniil Pryima <swimmin2@gmail.com>
2021-08-19 11:17:20 +0300

    Removed old database

--- TXDatabaseShared/NetworkEvents/Security/LoginSuccessEvent.cs
+++
@@ -1,8 +0,0 @@
-using Simple.Net;
-
-namespace TXDatabase.NetworkEvents.Security {
-    public struct LoginSuccessEvent : INetSerializable {
-        public void Serialize(NetWriter writer) {}
-        public void Deserialize(NetReader reader) {}
-    }
-}
--- TXDatabaseShared/Simple.Net/INetSerializable.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Simple.Net {
-    public interface INetSerializable {
-        void Serialize(NetWriter buffer);
-        void Deserialize(NetReader buffer);
-    }
-}
